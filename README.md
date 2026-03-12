4f6c21685e59f92fb18d16b8be00ab6871124ee5
philippark <philippark271@gmail.com>
2026-03-12 01:40:39 -0400

    bit_strings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My solutions to https://cses.fi/problemset/list/
+
+34/400
---
+++ introductory/bit_strings.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long MOD = 1000000007;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+
+    long long ret = 2;
+    while (--n) {
+        ret = (ret*2) % MOD;
+    }
+
+    cout << ret%MOD << "\n";
+
+    return 0;
+}

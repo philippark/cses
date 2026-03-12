3f7e4f97e27a8181390fccb6ea3d2b499a05487c
philippark <philippark271@gmail.com>
2026-03-12 19:40:09 -0400

    palindrom reorder

---
+++ introductory/palindrome_reorder.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    vector<int> v(26, 0);
+    for (auto c : s) {
+        v[c-'A']++;
+    }
+
+    std::string left="", mid="", right="";
+    char ch = 'A';
+    for (auto i : v) {
+        if (i%2 != 0) {
+            if (mid != "") {
+                cout << "NO SOLUTION\n";
+                return 0;
+            }
+            mid = string(i, ch);
+        } else {
+            left += string(i/2, ch);
+            right += string(i/2, ch);
+        }
+        ch += 1;
+    }
+    reverse(right.begin(), right.end());
+    cout << left + mid + right << "\n";
+
+    return 0;
+}

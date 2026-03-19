9079a0109235412f12a989ca410b9a4e5f8273c0
philippark <philippark271@gmail.com>
2026-03-19 19:34:54 -0400

    apple division

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 My solutions to https://cses.fi/problemset/list/
 
-40/400
+41/400
---
+++ introductory/apple_division.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+
+#define PB push_back
+
+void explore(vll &v, int start, ll L, ll R, ll& ret) {
+    if (start == v.size()) {
+        if (R < 0 || L < 0) {
+            return;
+        }
+        if (ret == -1) {
+            ret = abs(R-L);
+        }
+        else ret = min(ret, abs(R-L));
+        return;
+    }
+
+    explore(v, start+1, L+v[start], R, ret);
+    explore(v, start+1, L, R+v[start], ret);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vll v(n);
+    for (auto& x : v) {
+        cin >> x;
+    }
+
+    ll ret = -1;
+    explore(v, 0, 0, 0, ret);
+    cout << ret << "\n";
+
+    return 0;
+}

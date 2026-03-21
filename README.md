f8a05d02f9519883f147065e4199b8b1869b5bcc
philippark <philippark271@gmail.com>
2026-03-21 19:31:55 -0400

    chessboard and queens

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 My solutions to https://cses.fi/problemset/list/
 
-42/400
+43/400
--- introductory/chessboard_and_queens.cc
+++ introductory/chessboard_and_queens.cc
@@ -1,26 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-just explore all possibilities.
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
 
-go through, if can place, place. explore. unplace. 
+#define PB push_back 
 
-when place a queen. mark that row and col as used.
-rows, cols vectors of 8. 
+bool can_place(int r, int c, vvi& placed) {
+    for (auto v : placed) {
+        if (v[0] == r || v[1] == c) {
+            return false;
+        }
 
-if can place:
-not *
-and rows[r] and cols[c] is not 1.
+        if (abs(r-v[0]) == abs(c-v[1])) {
+            return false;
+        }
+    }
 
-can just go through each row
+    return true;
+}
 
-explore()
-*/
+int explore(vs& grid, int start_row, vvi& placed, int q) {
+    if (q == 0) {
+        return 1;
+    }
+    
+    int ret = 0;
+    for (int i = start_row; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if (grid[i][j] == '*'){
+                continue;
+            }
+            
+            if (can_place(i, j, placed)) {
+                placed.PB({i, j});
+                ret += explore(grid, i+1, placed, q-1);
+                placed.pop_back();
+            }
+        }
+    }
+
+    return ret;
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    vs grid(8);
+    for (auto &r : grid) {
+        cin >> r;
+    }
 
+    vvi placed;
+    int ret = explore(grid, 0, placed, 8);
+
+    cout << ret << "\n";
+
+    return 0;
 }

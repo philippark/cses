b3d2099297959108dae2a39faff9bbed41e1d7a5
philippark <philippark271@gmail.com>
2026-04-15 01:17:18 -0400

    towers optimized

--- sorting_and_searching/towers.cc
+++ sorting_and_searching/towers.cc
@@ -2,59 +2,38 @@
 Idea:
 Choose to add to the stack with the minimum difference
 this is to preserve space. A greedy + binary searching solution.
+
+1st attempt: binary search
+2nd attempt: multiset
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef vector<ll> vll;
-
-#define PB push_back
-
-int bin_search(vll& v, int t) {
-    int L = 0, R = v.size()-1;
-    while (L <= R) {
-        int mid = L + (R-L)/2;
-        if (v[mid] <= t) {
-            L = mid+1;
-        } else {
-            R = mid-1;
-        }
-    }
-
-    if (R < 0) return 0;
-    if (L >= v.size()) return v.size();
-
-    if (v[R] > t) {
-        return R;
-    } 
-    return L;
-}
 
 int main() {
     int n;
     cin >> n;
 
-    vll v;
+    int arr[n];
+    for (auto&x : arr) {
+        cin >> x;
+    }
+
+    multiset<int> ms;
+    int piles = 0;
     for (int i = 0; i < n; ++i) {
-        ll k;
-        cin >> k;
-        if (v.size()==0) {
-            v.push_back(k);
-            continue;
-        }
-
-        int idx = bin_search(v, k);
-    
-        if (idx == v.size()) {
-            v.PB(k);
+        auto it = ms.upper_bound(arr[i]);
+        if (it != ms.end()) {
+            ms.erase(it);
+            ms.insert(arr[i]);
         } else {
-            swap(v[idx], k);
+            ms.insert(arr[i]);
+            ++piles;
         }
-    } 
+    }
 
-    cout << v.size() << "\n";
+    cout << piles << "\n";
 
     return 0;
 }

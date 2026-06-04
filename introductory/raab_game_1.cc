a3394da4ab7ad2d7d17c1703ebf45a5b925dd912
philippark <philippark271@gmail.com>
2026-06-03 21:11:46 -0400

    raab game 1

--- introductory/raab_game_1.cc
+++ introductory/raab_game_1.cc
@@ -1,64 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
 typedef vector<int> vi;
 
 #define PB push_back
+#define sort(a, ...) sort(a.begin(), a.end(), ##__VA_ARGS__)
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+#define M 1000000007
+#define INF 2147483647
+#define INFL 9223372036854775807
+#define MOD 998244353
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
     int t;
-    cin >> t;
+    cin>>t;
+    while(t--) {
+        int n, a, b;
+        cin>>n>>a>>b;
 
-    while (t--) {
-        int n, a_w, b_w;
-        cin >> n >> a_w >> b_w;
-
-        int d = n - (a_w + b_w);
-
-        if (d < 0) {
+        if (a+b > n || (a==0 && b>0) || (b==0 && a>0)) {
             cout << "NO\n";
             continue;
         }
+        cout << "YES\n";
 
-        int a_l = n - (a_w + d);
-        int b_l = n - (b_w + d);
-       
-        cout << "-------\n";
-        cout << a_w << " " << a_l << " " << d << "\n";
-        cout << b_w << " " << b_l << " " << d << "\n";
-        cout << "-------\n";
-        
-        if (a_w != b_l || a_l != b_w) {
-            cout << "NO\n";
-            continue;
-        }
+        int d = n-(a+b);
+        for (int i = 1; i <= n; ++i) cout << i << " ";
+        cout << "\n";
 
-        if ((a_w + a_l + d) != n) {
-            cout << "NO\n";
-            continue;
-        }
-
-        vi a_cards;
-        int i = n;
-        for (int j = 0; j < a_w; ++j) {
-            a_cards.PB(i);
-            i--;
-        }
-        for (int j = 0; j < d; ++j) {
-            a_cards.PB(i);
-            i--;
-        }
-        for (int j = 0; j < a_l; ++j) {
-            a_cards.PB(i);
-            i--;
-        }
-
-        for (auto z : a_cards) {
-            cout << z << " ";
-        }
+        for (int i = 1; i <= d; ++i) cout << i << " ";
+        for (int i = d+a+1; i <= n; ++i) cout << i << " ";
+        for (int i = d+1; i <= d+a; ++i) cout << i << " ";
         cout << "\n";
     }
 
